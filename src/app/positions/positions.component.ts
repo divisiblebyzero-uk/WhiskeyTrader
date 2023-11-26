084f5eb81c4bcd821992a7ae9091585f2720da12
Matthew Smalley <dev@divisiblebyzero.co.uk>
2023-11-26 11:27:44 +0000

    positions works now

--- src/app/positions/positions.component.ts
+++ src/app/positions/positions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { ColDef, GridOptions, RowClassParams, ValueFormatterParams, ValueGetterParams } from 'ag-grid-community';
 import { DatePickerRendererComponent } from '../cellRenderers/date-picker-renderer/date-picker-renderer.component';
@@ -9,64 +9,40 @@ import { WhiskeyTradesService } from '../Data/whiskey-trades.service';
 import { WhiskeysService } from '../Data/whiskeys.service';
 import { Whiskey, WhiskeyPosition } from '../entities';
 import { WhiskeyPositionCalculatorService } from '../whiskey-position-calculator.service';
+import { Table } from 'primeng/table';
 
+interface WhiskeyPositionWithDetails extends WhiskeyPosition {
+  whiskeyName: string,
+  distiller: string,
+}
 @Component({
   selector: 'app-positions',
   templateUrl: './positions.component.html',
   styleUrls: ['./positions.component.scss']
 })
 export class PositionsComponent implements OnInit {
+  @ViewChild('positionsTable') positionsTable!: Table
+  loading: boolean = true;
+  error: any;
 
   faPlus = faPlus;
 
   // With help from https://www.codeproject.com/Articles/5266363/agGrid-for-Angular-The-Missing-Manual
 
-  constructor(private whiskeysService:WhiskeysService, private whiskeyTradesService: WhiskeyTradesService, private calc: WhiskeyPositionCalculatorService) {
+  constructor(private whiskeysService: WhiskeysService, private calc: WhiskeyPositionCalculatorService) {
   }
 
-  rowData: WhiskeyPosition[] | null = null;
-  whiskeys: Whiskey[] | null = null;
+  rowData!: WhiskeyPosition[];
+  whiskeys!: Whiskey[];
 
-  columnDefs: ColDef[] = [
-    { headerName: 'Whiskey Name', field: 'whiskeyId',
-      cellEditor: 'dropDownListRendererComponent', cellEditorParams: this.whiskeys?this.whiskeys.filter(w => w.active):null,
-      valueGetter: (params: ValueGetterParams) => this.whiskeys?.find(w => w.id == params.data.whiskeyId)?.name,
-      minWidth: 130,
-      maxWidth: 200
-    },
-    { headerName: '#', field: 'numberOfBottles', pinned: 'left', minWidth: 50, maxWidth: 50 },
-    { headerName: 'Buy', field: 'totalPurchases', valueFormatter: this.currencyFormatter, minWidth: 75, maxWidth: 75 },
-    { headerName: 'Sell', field: 'totalSales', valueFormatter: this.currencyFormatter, minWidth: 75, maxWidth: 75 },
-    { headerName: 'Avg Price', field: 'averagePricePerBottle', valueFormatter: this.currencyFormatter, minWidth: 100, maxWidth: 100 },
-    { headerName: 'Mkt Price', field: 'currentMarketPricePerBottle', valueFormatter: this.currencyFormatter, minWidth: 100, maxWidth: 100 },
-    { field: 'profit', valueFormatter: this.currencyFormatter },
-    { field: 'profitPerBottle', valueFormatter: this.currencyFormatter },
-    { headerName: 'RoI', field: 'returnOnInvestment', valueFormatter: this.percentageFormatter },
-    { headerName: 'Open?', field: 'openPosition', valueFormatter: this.booleanFormatter }
-  ];
-
-  gridOptions: GridOptions = {
-    getRowStyle: (params: RowClassParams) => { return params.data.openPosition? {background: 'pink'}: {background: 'powderblue'} },
-    defaultColDef: {
-      resizable: true,
-      sortable: true,
-      filter: true,
-      editable: false,
-      minWidth: 75,
-      maxWidth: 75
-    },
-    onFirstDataRendered: this.onFirstDataRendered,
-    components: { 
-      dateTimeRenderer: DateTimeRenderer,
-      dropDownListRendererComponent: DropDownListRendererComponent,
-      deleteButtonRendererComponent: DeleteButtonComponent,
-      datePickerRendererComponent: DatePickerRendererComponent
-     },
-     context: { componentParent: this }
-  };
-
-  onFirstDataRendered(params:any) {
-    params.api.sizeColumnsToFit();
+  addWhiskeyDetails(position: WhiskeyPosition): WhiskeyPositionWithDetails {
+    const w: Whiskey | undefined = this.whiskeys?.find(w => w.id == position.whiskeyId);
+    if (w) {
+      return { ...position, whiskeyName: w.name, distiller: w.distiller }
+    } else {
+      console.log("Error - whiskey not found: " + position.whiskeyId)
+      return { ...position, whiskeyName: "", distiller: "" }
+    }
   }
 
   ngOnInit(): void {
@@ -74,24 +50,22 @@ export class PositionsComponent implements OnInit {
   }
 
   getWhiskeyPositions(): void {
-    this.calc.getPositions().then(positions => this.rowData = positions);
-    this.whiskeysService.list().subscribe(whiskeys => this.whiskeys = whiskeys);
+    this.whiskeysService.list().subscribe(whiskeys => {
+      this.whiskeys = whiskeys
+      this.calc.getPositions().then(positions => {
+        this.rowData = positions.map(p => this.addWhiskeyDetails(p))
+        this.loading = false
+      });
+    });
   }
 
-  currencyFormatter(params: ValueFormatterParams): any {
-    return '\xA3' + Number(params.value).toFixed(2)
-    .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+
+  clear(table: Table) {
+    table.clear();
   }
 
-  percentageFormatter(params: ValueFormatterParams): any {
-    if (params.value && !Number.isNaN(params.value) && Number.isFinite(params.value)) {
-      return Math.floor((Number(params.value)*100)) + "%";
-    } else { 
-      return "";
-    }
-  }
-
-  booleanFormatter(params: ValueFormatterParams): any {
-    return params.value?'Yes':'No';
+  globalFilterUpdate(event: any) {
+    console.log(JSON.stringify(event.target.value))
+    this.positionsTable.filterGlobal(event.target.value, 'contains');
   }
 }
